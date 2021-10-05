28ad427164efcc574ef56c46743cfd0f764c9798
Derek Lewis <derek.lewis.den@outlook.com>
2021-10-05 15:35:40 -0600

    server.js and index.js in server file updated with boilerplate

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,15 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Profile {
+    _id: ID
+    name: String
+    email: String
+    password: String
+    skills: [String]!
+  }
+
+  type Auth {
+    token: ID!
+    profile: Profile
+  }
