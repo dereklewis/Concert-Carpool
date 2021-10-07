dd08929521f8e114ac003d34f58fd91512ed33d0
Derek Lewis <derek.lewis.den@outlook.com>
2021-10-07 15:58:31 -0600

    created Event seed file

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,11 +1,24 @@
 const db = require("../config/connection");
-const { Profile } = require("../models");
+const { Profile, Event } = require("../models");
 const profileSeeds = require("./profileSeeds.json");
+const eventSeeds = require("./eventSeeds.json");
 
 db.once("open", async () => {
   try {
     await Profile.deleteMany({});
     await Profile.create(profileSeeds);
+
+    console.log("all done!");
+    process.exit(0);
+  } catch (err) {
+    throw err;
+  }
+});
+
+db.once("open", async () => {
+  try {
+    await Event.deleteMany({});
+    await Event.create(eventSeeds);
 
     console.log("all done!");
     process.exit(0);
