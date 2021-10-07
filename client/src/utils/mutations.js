8d35ff246ffae7c9a8a2e152f1e5e55a9c01406c
Derek Lewis <derek.lewis.den@outlook.com>
2021-10-07 13:28:56 -0600

    changed typedefs

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,12 +1,31 @@
 import { gql } from "@apollo/client";
 
 export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!, $email: String!, $password: String!) {
-    addProfile(name: $name, email: $email, password: $password) {
+  mutation addProfile(
+    $name: String!
+    $email: String!
+    $password: String!
+    $city: String!
+    $neighborhood: String!
+    $aboutme: String!
+  ) {
+    addProfile(
+      name: $name
+      email: $email
+      password: $password
+      city: $city
+      neighborhood: $neighborhood
+      aboutme: $aboutme
+    ) {
       token
       profile {
         _id
         name
+        email
+        password
+        city
+        neighborhood
+        aboutme
       }
     }
   }
