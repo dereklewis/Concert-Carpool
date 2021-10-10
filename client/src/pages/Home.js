9d3bdda7a66bb5d1abf251dbfa938b7d5587836f
Derek Lewis <derek.lewis.den@outlook.com>
2021-10-10 13:04:41 -0600

    hero image finalized

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
-
-import YourImage from "../images/hero.png";
-import "../App";
+import "../App.css";
+import { Container } from "react-bootstrap";
 
 const Home = () => {
   return (
-    <div className="d-flex justify-content-center ">
-      <img className="p-2" src={YourImage} />
-    </div>
+    <div className="container-fluid homepage-bgimage "></div>
+
+    // <div className="d-flex justify-content-center ">
+    //   <img className="p-2" src={YourImage} />
+    // </div>
   );
 };
 
